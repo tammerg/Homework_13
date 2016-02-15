4b488defd6f21b6c95419e21d805f7eefba0f7a5
Tammerg <tammergalal20@gmail.com>
2016-02-15 16:55:40 -0500

    added config folder and db folder, created databases required and beginning to work on ORM

---
+++ burger/config/connection.js
@@ -0,0 +1,20 @@
+module.exports = function connection (){
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  port: 3306,
+  host : 'localhost',
+  user : 'root',
+  password : '',
+  database : 'burgers_db'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, data) {
+  if (err) throw err;
+
+  console.log('The solution is: ', data);
+});
+
+connection.end();
+}
---
+++ burger/config/orm.js
@@ -0,0 +1,4 @@
+var orm = {
+
+}
+module.exports = orm;
