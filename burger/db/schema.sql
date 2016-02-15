e6ef232d346bda176ba93d322c344c6c238f9b51
Tammerg <tammergalal20@gmail.com>
2016-02-15 15:17:11 -0500

    created package.json and downloaded all dependencies, created server.js and also a schema file.

---
+++ burger/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  burger_name varchar(255),
+  devoured BOOL,
+  date TIMESTAMP
+);
