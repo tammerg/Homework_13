5e01ad8874494e228bb2ac8f323919781375aa44
tammerg <tammergalal20@gmail.com>
2016-03-09 13:22:22 -0500

    still working on deployment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   port: 3306,
-  host : 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user : 'vd9ql3qe45w11uy2',
-  password : 'aaiiygito6gm5ce6',
-  database : 'oafe1ud6yohijc7k'
+  host : 'localhost',
+  user : 'root',
+  password : '',
+  database : 'burgers_db'
 });
 
 
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 var routes = require('./controllers/burgers_controller.js');
-var dot = require('dotenv').config();
-var PORT = process.env.PORT || 8080;
+require('dotenv').config();
+var PORT = process.env.PORT || 3306;
 var app = express();
 
 app.use('/scripts', express.static('public/scripts'));
